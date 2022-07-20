6a95c6249eded883bea276f55f06f720c6ebb5af
jzstarpath <106030464+jzstarpath@users.noreply.github.com>
2022-07-20 13:34:04 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='xcontrol',
+    version='0.1.0',    
+    description='Package for X controller',
+    author='Saurav',
+    author_email='saurav@starpath.space',
+    license='MIT',
+    packages=['xcontrol'],
+    keywords=['xcontrol', 'motor', 'motor control'],
+    install_requires=[
+
+    ],
+)
---
+++ xcontrol/__init__.py
@@ -0,0 +1,6 @@
+"""
+Library for X Controller by Precision Robotics.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Saurav Shroff"
